ae827bac40696e92b946b0894f3f9b16068a0284
Jussi Hietanen <jussi.hietanen@iki.fi>
2020-12-29 11:02:13 +0200

    Add knight legal move check

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -15,5 +15,36 @@ Knight::~Knight()
 
 std::vector<int> Knight::GetLegalMoves(const std::array<std::unique_ptr<Piece>, 64>& w_board, PiecePosition w_pos, PieceMove w_last_move) const
 {
-  return std::vector<int>();
+  std::vector<int> legal_moves;
+
+  // List of candidate moves
+  std::vector<int> candidate_moves = { 6, 15, 17, 10, -6, -15, -17, -10 };
+
+  
+  for(auto it = candidate_moves.begin(); it != candidate_moves.end();)
+  {
+    // Boundary checks
+    if( w_pos+*it > 63
+    || w_pos+*it < 0
+    || std::abs( (w_pos % 8) - ((w_pos+*it) % 8) > 2) )
+    {
+      std::cout << "pilalla" << std::endl;
+      it = candidate_moves.erase(it);
+    }
+    else
+    {
+      // Check if the tile is preoccupied by same color
+      if(w_board[w_pos+*it]->GetColor() == _color)
+      {
+        it = candidate_moves.erase(it);
+      }
+      else
+      {
+        legal_moves.push_back(w_pos+*it);
+        it++;
+      }
+    }
+  }
+
+  return legal_moves;
 }
--- src/Knight.hpp
+++ src/Knight.hpp
@@ -9,7 +9,7 @@ public:
 Knight(Piece::Color w_color = None);
 ~Knight();
 
-virtual std::vector<int> GetLegalMoves(const std::array<std::unique_ptr<Piece>, 64>& w_board, PiecePosition w_pos, PieceMove w_last_move) const;
+std::vector<int> GetLegalMoves(const std::array<std::unique_ptr<Piece>, 64>& w_board, PiecePosition w_pos, PieceMove w_last_move) const;
 
 private:
 
