26f12b4a10f856ef4d0aeb95f9fdda495a4a2d04
Jussi Hietanen <jussi.hietanen@iki.fi>
2020-12-29 10:40:09 +0200

    Add legal move checking for rooks

--- src/Rook.cpp
+++ src/Rook.cpp
@@ -15,5 +15,103 @@ Rook::~Rook()
 
 std::vector<int> Rook::GetLegalMoves(const std::array<std::unique_ptr<Piece>, 64>& w_board, PiecePosition w_pos, PieceMove w_last_move) const
 {
-  return std::vector<int>();
+  std::vector<int> legal_moves;
+
+  // Get vertical moves downwards
+  for(int i = -8; i > -64; i-=8)
+  {
+    // Boundary check
+    if(w_pos + i < 0)
+    {
+      break;
+    }
+
+    // Check if the tile is occupied
+    if(w_board[w_pos+i]->GetType() == Piece::Type::Empty)
+    {
+      legal_moves.push_back(w_pos+i);
+    }
+    else
+    {
+      if(w_board[w_pos+i]->GetColor() != _color)
+      {
+        legal_moves.push_back(w_pos+i);
+      }
+      break;
+    }
+  }
+
+  // Get vertical moves upwards
+  for(int i = 8; i < 64; i+=8)
+  {
+    // Boundary check
+    if(w_pos + i >= 64)
+    {
+      break;
+    }
+
+    // Check if the tile is occupied
+    if(w_board[w_pos+i]->GetType() == Piece::Type::Empty)
+    {
+      legal_moves.push_back(w_pos+i);
+    }
+    else
+    {
+      if(w_board[w_pos+i]->GetColor() != _color)
+      {
+        legal_moves.push_back(w_pos+i);
+      }
+      break;
+    }
+  }
+
+  // Get horizontal moves to the left
+  for(int i = -1; i > -8; i--)
+  {
+    // Row check
+    if(((w_pos + i) / 8) != (w_pos / 8) || w_pos + i < 0)
+    {
+      break;
+    }
+
+    // Check if the tile is occupied
+    if(w_board[w_pos+i]->GetType() == Piece::Type::Empty)
+    {
+      legal_moves.push_back(w_pos+i);
+    }
+    else
+    {
+      if(w_board[w_pos+i]->GetColor() != _color)
+      {
+        legal_moves.push_back(w_pos+i);
+      }
+      break;
+    }
+  }
+
+  // Get horizontal moves to the right
+  for(int i = 1; i < 8; i++)
+  {
+    // Row check
+    if(((w_pos + i) / 8) != (w_pos / 8) || w_pos + i >= 64)
+    {
+      break;
+    }
+
+    // Check if the tile is occupied
+    if(w_board[w_pos+i]->GetType() == Piece::Type::Empty)
+    {
+      legal_moves.push_back(w_pos+i);
+    }
+    else
+    {
+      if(w_board[w_pos+i]->GetColor() != _color)
+      {
+        legal_moves.push_back(w_pos+i);
+      }
+      break;
+    }
+  }
+
+  return legal_moves;
 }
