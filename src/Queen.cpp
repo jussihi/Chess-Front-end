a6e11b52f333961e4e667429290ccc7fb679c543
Jussi Hietanen <jussi.hietanen@iki.fi>
2020-12-29 15:05:41 +0200

    Add queen legal move checks

--- src/Queen.cpp
+++ src/Queen.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 
 Queen::Queen(Piece::Color w_color) :
-Piece(w_color)
+Piece(w_color),
+_r(_color),
+_b(_color)
 {
   _type = Piece::Type::Queen;
   std::cout << "Created new Queen with color " << _color << std::endl;
@@ -15,5 +17,11 @@ Queen::~Queen()
 
 std::vector<int> Queen::GetLegalMoves(const std::array<std::unique_ptr<Piece>, 64>& w_board, PiecePosition w_pos, PieceMove w_last_move) const
 {
-  return std::vector<int>();
+
+  std::vector<int> rook_moves = _r.GetLegalMoves(w_board, w_pos, w_last_move);
+  std::vector<int> bishop_moves = _b.GetLegalMoves(w_board, w_pos, w_last_move);
+
+  std::move(rook_moves.begin(), rook_moves.end(), std::back_inserter(bishop_moves));
+
+  return bishop_moves;
 }
--- src/Queen.hpp
+++ src/Queen.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
-
 #include "Piece.hpp"
+
+/* Borrow legal move checking algos from rook and bishop */
+#include "Rook.hpp"
+#include "Bishop.hpp"
 
 class Queen : public Piece
 {
@@ -9,8 +12,9 @@ public:
 Queen(Piece::Color w_color = None);
 ~Queen();
 
-virtual std::vector<int> GetLegalMoves(const std::array<std::unique_ptr<Piece>, 64>& w_board, PiecePosition w_pos, PieceMove w_last_move) const;
+std::vector<int> GetLegalMoves(const std::array<std::unique_ptr<Piece>, 64>& w_board, PiecePosition w_pos, PieceMove w_last_move) const;
 
 private:
-
+::Rook _r;
+::Bishop _b;
 };
